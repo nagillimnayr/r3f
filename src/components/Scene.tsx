62ff1f1eed23eda6a17efeb4ff87603946f5d124
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-14 10:22:24 -0700

    feat: add SphereMesh component to Scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CubeMesh from './CubeMesh';
+import { SphereMesh } from './SphereMesh';
 
 const Scene = () => {
   return (
     <>
-      <CubeMesh color={'orange'} />
+      <CubeMesh color={'orange'} position={[4, 0, 0]} />
+      <SphereMesh color={'skyblue'} position={[-4, 0, 0]} />
     </>
   );
 };
