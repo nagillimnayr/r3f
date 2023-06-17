fc72beed869218e9d49ac4c3d5c130500ee7ef1e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-16 17:30:12 -0700

    feat: resize divs with tailwind

--- src/components/CanvasHolder.tsx
+++ src/components/CanvasHolder.tsx
@@ -1,7 +1,9 @@
 import { Canvas } from '@react-three/fiber';
 import React, { useState } from 'react';
 import CubeMesh from './CubeMesh';
+import { VRButton, XR } from '@react-three/xr';
 import {
+  CameraControls,
   Grid,
   OrbitControls,
   PerspectiveCamera,
@@ -13,17 +15,18 @@ import Scene from './Scene';
 
 const CanvasHolder = () => {
   return (
-    <div className="canvas-outer-box">
-      <div className="canvas-inner-box">
+    <div className="min-w-96 min-h-fit w-[1200px] flex flex-col justify-start items-center border-2 border-red-500">
+      <div className="min-w-full h-[800px] border-2 border-blue-500">
         <Canvas gl={{ logarithmicDepthBuffer: true }}>
-          <Stats />
-          <PerspectiveCamera makeDefault position={[0, 0, 10]}>
-            <spotLight />
-          </PerspectiveCamera>
-          <OrbitControls />
+          <XR>
+            <Stats />
 
-          <Scene />
+            <Scene />
+          </XR>
         </Canvas>
+      </div>
+      <div className="relative flex flex-col place-items-center w-36 h-24 border-2 bg-slate-500 border-green-400">
+        <VRButton />
       </div>
     </div>
   );
